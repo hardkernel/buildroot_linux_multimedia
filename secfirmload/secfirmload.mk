9ed6b762ebeb5eb13e5e5ba0bf0e13390b6fa16c
baoguo.chen <baoguo.chen@amlogic.com>
2018-11-13 17:11:12 +0800

    secfirmload: change the config files [1/1]
    PD#SWPL-99
    
    Problem:
    Config files are wrong.
    
    Solution:
    1.change the config files.
    2.change directory structure.
    
    Verify:
    P215 + Linux trunk
    
    Change-Id: Ieb7dca1f38af7113e2aec03be6ae9bc955239e1d
    Signed-off-by: baoguo.chen <baoguo.chen@amlogic.com>

--- secfirmload/secfirmload.mk
+++ secfirmload/secfirmload.mk
@@ -3,32 +3,23 @@
 #secure firmware load
 #
 ################################################################################
-ifeq ($(BR2_PACKAGE_SECFIRMLOAD),y)
-SECVIDEO_FIRMWARE_VERSION = 1.0
-#prebuilt defines.
-SECVIDEO_PREBUILT_SITE = $(TOPDIR)/../multimedia/secfirmload/secloadbin
-SECVIDEO_PREBUILT_SITE_METHOD = local
 
-ifeq ($(BR2_aarch64),y)
-SECVIDEO_PREBUILT_DIRECTORY = multimedia/secfirmload/secloadbin/arm64
-else
-SECVIDEO_PREBUILT_DIRECTORY = multimedia/secfirmload/secloadbin/arm
-endif
+SECFIRMLOAD_VERSION = 1.0
+SECFIRMLOAD_SITE = $(TOPDIR)/../multimedia/secfirmload/secloadbin
+SECFIRMLOAD_SITE_METHOD = local
 
 define SECFIRMLOAD_INSTALL_TARGET_CMDS
-	cp -av $(TOPDIR)/../$(SECVIDEO_PREBUILT_DIRECTORY)/tee_preload_fw     $(TARGET_DIR)/usr/bin/tee_preload_fw
-	cp -av $(TOPDIR)/../$(SECVIDEO_PREBUILT_DIRECTORY)/tee_preload_fw.so  $(TARGET_DIR)/usr/lib/tee_preload_fw.so
-	cp -av $(TOPDIR)/../$(SECVIDEO_PREBUILT_DIRECTORY)/526fc4fc-7ee6-4a12-96e3-83da9565bce8.ta   $(TARGET_DIR)/lib/teetz/
+	$(INSTALL) -D -m 755 $(@D)/$(BR2_ARCH)/tee_preload_fw     $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -D -m 644 $(@D)/$(BR2_ARCH)/tee_preload_fw.so  $(TARGET_DIR)/usr/lib/
+	$(INSTALL) -D -m 644 $(@D)/$(BR2_ARCH)/526fc4fc-7ee6-4a12-96e3-83da9565bce8.ta   $(TARGET_DIR)/lib/teetz/
+
+	$(SECVIDEO_PREBUILT_INSTALL_INIT_SYSV)
 endef
 
 ifeq ($(BR2_PACKAGE_LAUNCHER_USE_SECFIRMLOAD), y)
 define SECVIDEO_PREBUILT_INSTALL_INIT_SYSV
-	rm -rvf $(TARGET_DIR)/etc/init.d/S60*
-	$(INSTALL) -D -m -v 755 multimedia/secfirmload/secloadbin/S60secload $(TARGET_DIR)/etc/init.d/S60secload
+	$(INSTALL) -D -m 755 $(@D)/S60secload $(TARGET_DIR)/etc/init.d/
 endef
 endif
 
 $(eval $(generic-package))
-
-endif
-
